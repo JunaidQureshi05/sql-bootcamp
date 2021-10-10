53a42e40789270c690487ea7275ab0b6d7c08609
Junaid Qureshi <Junaid.562000@gmail.com>
2021-10-10 10:21:05 +0530

    add

---
+++ 3.database.sql
@@ -0,0 +1,15 @@
+
+SHOW DATABASES;
+--List all the databases present in the database server.
+
+CREATE DATABASE <name>;
+--creates new database in the dtabase server.
+
+DROP DATABASE <name>;
+--Deletes database from the database server.
+
+USE <name>;
+--switch u to a specified database to work with
+
+SELECT DATABASE();
+--Gives us the name of database on which we are currently working on.
